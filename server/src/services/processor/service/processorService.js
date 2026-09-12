e0e348ad438677e9a2a2fa05c3fa7208d12884c0
ChitvanGarg2000 <chitvangarg0305@gmail.com>
2026-09-12 18:34:32 +0530

    completed processor service

--- server/src/services/processor/service/processorService.js
+++ server/src/services/processor/service/processorService.js
@@ -1,18 +1,18 @@
 import logger from '../../../shared/config/logger.js'
 
 export class ProcessorService {
-    constructor({apiHitsRepository, metricsRepository}){
-        if(!apiHitsRepository || !metricsRepository){
+    constructor({ apiHitsRepository, metricsRepository }) {
+        if (!apiHitsRepository || !metricsRepository) {
             throw new Error('apiHitsRepository and metricsRepository are required')
         }
         this.apiHitsRepository = apiHitsRepository
         this.metricsRepository = metricsRepository
     }
 
-    getTimeBucket = async (timeStamp, interval='hour') => {
+    getTimeBucket = async (timeStamp, interval = 'hour') => {
         const date = new Date(timeStamp)
 
-        switch(interval){
+        switch (interval) {
             case 'hour':
                 return date.setMinutes(0, 0, 0)
             case 'day':
@@ -30,6 +30,50 @@ export class ProcessorService {
         return this.metricsRepository.getOverallStats(clientId, startTime, endTime)
     }
 
+    _updateMetricsWithFallback = async (eventData) => {
+        // Calculate time bucket 
+
+        try {
+            const timeBucket = this.getTimeBucket(eventData.timestamp, "hour")
+            const metricsData = {
+                clientId: eventData.clientId.toString(),
+                serviceName: eventData.serviceName,
+                endpoint: eventData.endpoint,
+                method: eventData.method,
+                totalHits: 1,
+                errorHits: eventData.statusCode > 400 ? 1 : 0,
+                avgLatency: eventData.latencyMs,
+                minLatency: eventData.latencyMs,
+                maxLatency: eventData.latencyMs,
+                timeBucket,
+            }
+    
+            await this.metricsRepository.upsertEndpointMetrics(metricsData)
+        }catch(error){
+            logger.error('ProcessorService: failed to update metrics with fallback', {
+                error: error.message,
+                eventId: eventData.id
+            })
+            throw error
+        }
+       
+    }
+
+    cleanUpEvent = async (daysToKeep = 30) => {
+        try {
+            let cutoffDate = new Date()
+            cutoffDate.setDate(cutoffDate.getDate() - daysToKeep)
+            await this.apiHitsRepository.deleteOldEvents(cutoffDate)
+            logger.info('ProcessorService: cleaned up old events', { cutoffDate })
+        } catch (error) {
+            logger.error('ProcessorService: failed to clean up old events', {
+                error: error.message,
+                daysToKeep: daysToKeep,
+            })
+            throw error
+        }
+    }
+    
     processEvent = async (event) => {
         let rawEventSaved = false;
         try {
@@ -40,14 +84,26 @@ export class ProcessorService {
             await this.apiHitsRepository.save(event)
             rawEventSaved = true;
 
-            logger.info('ProcessorService: saved event', {eventId: event.id})
+            logger.info('ProcessorService: saved event', { eventId: event.id })
 
             // STEP 2: do not fail the entire process if the metrics update fails
-            await this._updateMetricsWithFallback(eventData)
+            await this._updateMetricsWithFallback(event)
 
-            logger.info('ProcessorService: updated metrics', {eventId: event.eventId})
+            logger.info('ProcessorService: updated metrics', { eventId: event.eventId })
 
         } catch (error) {
+            if (!rawEventSaved) {
+                logger.error('ProcessorService: failed to save raw event', {
+                    error: error.message,
+                    eventId: event.id
+                })
+                throw error
+            }
+
+            logger.error('Non-critical error in ProcessorService: processing event', {
+                error: error.message,
+                eventId: event.id
+            })
         }
     }
 }
